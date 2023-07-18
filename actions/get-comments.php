c0f76f754aa8c3b3317b70cb9ce66e697de868ae
ebusy9 <g404ebus@gmail.com>
2023-07-18 14:33:18 +0200

    maj

--- actions/get-comments.php
+++ actions/get-comments.php
@@ -1,3 +1,18 @@
 <?php
 
 $data = json_decode(file_get_contents('php://input'), true);
+$pictureId = $data['pictureId'];
+
+require 'connexion.php';
+
+try {
+    $prepareGetComment = $pdoChat->prepare("SELECT * FROM comments WHERE picutre_id = :picutre_id");
+    $prepareGetComment->execute([':picutre_id' => $pictureId]);
+    $fetchedComment = $prepareGetComment->fetchAll();
+} catch (PDOException $exception) {
+    $_SESSION['lastErrMsg'] = $exception->getMessage();
+    header('Location: ../signup.php?err=signupUsernameFetch');
+    exit();
+}
+
+echo json_encode($fetchedComment);
