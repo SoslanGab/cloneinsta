8cc39d3ad6726f2b05c6f9434a7858358c53358b
ebusy9 <g404ebus@gmail.com>
2023-07-19 15:02:50 +0200

    ajout boucle avec les posts sur profile.php

--- actions/get-pictures.php
+++ actions/get-pictures.php
@@ -13,8 +13,32 @@ try {
     exit();
 }
 
-echo "<pre>";
+
 foreach($fetchedPictures as $pic){
-    echo print_r($pic);
+echo<<<HTML
+    <article class="post">
+    <header>
+        <div class="title">
+            <h2><a href="user_profile.php?id={$pic['id']}">{$pic['img_title']}</a></h2>
+        </div>
+        <div class="meta">
+            <time class="published" datetime="2015-11-01">{$pic['timedate']}</time>
+            <a href="user_profile.php?id={$pic['id']}" class="author"><span class="name">{$pic['username']}</span><img src="../{$pic['pfpLink']}" alt="" /></a>
+        </div>
+    </header>
+    <a href="user_profile.php?id={$pic['id']}" class="image featured"><img src="{$pic['picture_link']}" alt="" /></a>
+    <p>{$pic['img_text']}</p>
+    <footer>
+        <ul class="actions">
+            <li><a href="commentaire.php" class="button large">Ajouter un commentaire</a></li>
+        </ul>
+        <ul class="stats">
+            <li><a href="#">General</a></li>
+            <li><a href="#" class="icon solid fa-heart">28</a></li>
+            <li><a href="#" class="icon solid fa-comment">128</a></li>
+        </ul>
+    </footer>
+</article>
+HTML;
 }
-echo "</pre>";
+
