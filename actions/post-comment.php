bbc3bd5eae913144c3e868616fa9863e409143bc
ebusy9 <g404ebus@gmail.com>
2023-07-21 14:15:01 +0200

    Des modifs

--- actions/post-comment.php
+++ actions/post-comment.php
@@ -5,6 +5,7 @@ session_start();
 $msgContent = htmlspecialchars($_POST['commentConent']);
 $pictureId = $_POST['pictureId'];
 $posterId = $_SESSION['idUser'];
+$lastPost = date("YYYY-MM-DD HH:MM:SS");
 
 
 require 'connection.php';
@@ -16,6 +17,20 @@ try {
         ':content' => $msgContent,
         ':poster_id' => $posterId,
         ':picture_id' => $pictureId
+    ]);
+    header('Location: ../profile.php?info=commentPosted');
+    exit();
+} catch (PDOException $exception) {
+    $_SESSION['lastErrMsg'] = $exception->getMessage();
+    header('Location: ../profile.php?err=postCommentFailed');
+    exit();
+}
+
+try {
+    $prepareInsertLastCommentUpdate = $pdoInsta->prepare("UPDATE users SET last_post = :last_post WHERE id = :id");
+    $prepareInsertLastCommentUpdate->execute([
+        ':last_post' => $msgContent,
+        ':id' => $posterId
     ]);
     header('Location: ../profile.php?info=commentPosted');
     exit();
