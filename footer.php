1954e56b254ac4bea6af761a3b1de402bfec5732
soslan <so.gabarayev@gmail.com>
2023-07-21 14:12:43 +0200

    Correction du footer + modification user-profile

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-    <div class="">
+    <div class="navbar sticky-bottom">
         <footer id="footer" class="py-3">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                 <li class="nav-item">
@@ -13,3 +13,21 @@
             <p class="text-center text-muted copyright">© copyright Soslan, Evgenii</p>
         </footer>
     </div>
+
+
+
+
+  
+</body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="assets/js/main.js" crossorigin="anonymous"></script>
+	<!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script src="assets/js/profile_custom_js.js"></script>
+</html>
